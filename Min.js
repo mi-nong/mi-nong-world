2a42d615b665ead5e90616b9e687f4daab2bd766
민영 <min@min-yeong-ui-MacBookPro.local>
2020-01-10 21:45:57 +0900

    first commit

---
+++ Min.js
@@ -0,0 +1,22 @@
+const express_box = require('express')
+const app = express_box()
+
+app.get('/', function (req, res) {
+    res.send("hello world!")
+})
+
+app.get('/min', function (req, res) {
+    res.send("해냈다~")
+})
+
+app.get('/babo', function (req, res) {
+    res.send("실패!")
+})
+
+// app.get('/', (req, res) => {
+//     res.send("hello world!")
+// })
+
+app.listen(3000, function () {
+    console.log("System is working")
+})
